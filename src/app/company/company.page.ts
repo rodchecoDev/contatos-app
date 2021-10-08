bdd50e7d2c2d3699137fb7e94e1a5cf80b1ebbae
Rodrigo Costa <rodrigo.cherubino@techrocks.com.br>
2021-10-08 16:27:59 -0300

    add company page

--- src/app/company/company.page.ts
+++ src/app/company/company.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Company } from '../models/company.model';
+import { CompanyService } from '../services/company.service';
 
 @Component({
   selector: 'app-company',
   templateUrl: 'company.page.html',
   styleUrls: ['company.page.scss']
 })
-export class CompanyPage {
+export class CompanyPage implements OnInit {
 
-  constructor() {}
+  companies: Company[] = [];
+
+  constructor(private companyService: CompanyService) {}
+
+  ngOnInit(): void {
+    this.companyService.getCompanies().subscribe(res => {
+      this.companies = res;
+    })
+  }
 
 }
